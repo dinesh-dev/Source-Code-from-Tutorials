a016eff199ce7e30602cba133e999eba669ca7b5
Dinesh <dinesh.sivam@gmail.com>
2016-09-25 12:43:55 -0400

    Update 37_javaBeginners.java

--- Java_Beginners/37_javaBeginners.java
+++ Java_Beginners/37_javaBeginners.java
@@ -19,7 +19,7 @@ public class tuna {
     public void setTime(int h, int m, int s){
         hour = ((h>=0 && h<24) ? h : 0);
         minute = ((m>=0 && m<60) ? m : 0);
-        hour = ((s>=0 && s<60) ? s : 0);
+        second = ((s>=0 && s<60) ? s : 0);
     }
     
     public  String toMilitary(){
